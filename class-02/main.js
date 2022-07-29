226898b807128b54c0c3c98da193260ccc104846
abdullah-mamun <aamun22@gmail.com>
2022-07-29 17:40:17 +0300

    class 02 wit selector and text menupulation

---
+++ class-02/main.js
@@ -0,0 +1,18 @@
+// text()
+
+// $("p").text("hasan");
+
+// html()
+// $("#name").html("<h1>Mamun</h1>");
+// $(".my-div h2").html("<h6>Abdullah</h6>")
+
+//apend()
+// $("#name").append("Hasan");
+// prepend
+// $("#name").prepend("Mohammad");
+
+// before
+
+var name = $("<p></p>").text("This is a paragraph")
+$("#p1").after(name);
+$("#p1").before(name);
