bd4cf46fa836fa2a65c6f579f94e07e51c675f74
abdullah-mamun <aamun22@gmail.com>
2022-07-29 20:23:36 +0300

    Attribute manupulation

---
+++ class-03/main.js
@@ -0,0 +1,15 @@
+// get attribute by Javascript
+// var attri = document.querySelector("a").getAttribute("href");
+// console.log(attri) /
+
+// Show / get attribute by jquery
+// $("a").attr("href");
+
+// Remove attribute by jquery
+
+// $("a").removeAttr("href");
+
+// ste attribute by JS
+// document.querySelector("a").setAttribute("href", "https://www.mamunferdousbd.com");
+// ste attribute by Jquery
+$("a").attr("href", "https://www.mamunferdousbd.com");
