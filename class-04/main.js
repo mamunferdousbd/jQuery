2e9950e928f5153f49c8faa8b125ab80bd5eddc1
abdullah-mamun <aamun22@gmail.com>
2022-07-29 20:45:05 +0300

    CSS Style Manupulation

---
+++ class-04/main.js
@@ -0,0 +1,12 @@
+// $("h2").css("color", "red")
+// $("h2").css("font-size", "3rem")
+// $("h2").css("background-color", "yellow")
+// $("h2").css("text-align", "center")
+// $("h2").css("text-transform", "uppercase")
+
+/*Json */
+
+// $("h2").css({ "color": "green", "font-size": "6rem", "background-color": "black", "text-align": "center", "text-transform": "uppercase" })
+// Add style by adding css classList
+// document.querySelector("h2").classList.add("style1")
+$("h2").addClass("style1 style2")
